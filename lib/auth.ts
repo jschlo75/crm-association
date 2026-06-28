72e06478d5e2d943e5fc3c50467e3cea05498700
jschlo75 <jschlosser@klint-consulting.com>
2026-06-28 14:59:01 +0200

    Session JWT limitée à 2 jours

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-  session: { strategy: "jwt" },
+  session: { strategy: "jwt", maxAge: 2 * 24 * 60 * 60 },
   pages: { signIn: "/login" },
   providers: [
     CredentialsProvider({
